596be8429820a571aad3507fc00ab411d7d3319c
NYUBNOOB <tinritto@gmail.com>
2024-10-03 23:10:48 +0700

    install mui

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import LoginPage from './page/login-page.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LoginPage />
   </StrictMode>,
 )
